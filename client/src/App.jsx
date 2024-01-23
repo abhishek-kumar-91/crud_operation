76f35e08ae88a6b4deb1a732285ba4aacc9626b0
Abhishek Kumar <abhishekkumar958830@gmail.com>
2024-01-23 15:11:35 +0530

    crud operation assignment done

---
+++ client/src/App.jsx
@@ -0,0 +1,136 @@
+import Popup from "./coponents/Popup";
+import { useState, useEffect } from "react";
+import {Pencil, Delete, Send} from "lucide-react"
+import axios from "axios";
+import Update from "./coponents/Update"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+function App() {
+  const [showModal, setShowModal] = useState(false);
+  const [updateModal, setUpdateModal] = useState(false);
+  const [data, setData] = useState(null);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [deleteId, setDeleteId] = useState("");
+  const [checkBox, setCheckBox] = useState(false)
+
+  const apiUrl = import.meta.env.MODE === 'development'
+  ? import.meta.env.VITE_DEV_API
+  : import.meta.env.VITE_PROD_API;
+
+  console.log("api url check", apiUrl )
+
+  const fetchData = () =>{
+    const result = axios.get(`${apiUrl}/api/user`).then((result) => setData(result.data))
+  }
+
+  useEffect(()=> {
+    
+    fetchData();
+  }, [deleteId])
+  console.log("result", data)
+
+
+  const handleUpdateClick = (user) => {
+    setSelectedUser(user);
+    setUpdateModal(true);
+    
+  };
+
+ const handleDelete = async(userId) =>{
+  const resData = await userId
+  setDeleteId(resData)
+  const response = await axios.delete(`${apiUrl}/api/user/${deleteId}`)
+  toast("Successfully Deleted!")
+  fetchData()
+ }
+
+ const handleSendData = async (user) => {
+  const resData = await user;
+
+  if (checkBox) {
+    const data = {
+      name: user.name,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      hobbies: user.hobbies
+    };
+
+    try {
+      const response = await axios.post(`${apiUrl}/api/user/send`, {
+        to: 'info@redpositive.in',
+        subject: 'Assignment Done',
+        text: JSON.stringify(data), // Convert data to a string
+      });
+
+      toast("email sent successfully!")
+    } catch (error) {
+      toast("Error during sending email!")
+    }
+  } else {
+    
+    toast("Please select checkbox!")
+  }
+};
+
+ 
+console.log("checkbox", checkBox)
+  
+  return (
+    <>
+      <div className="w-[100vw] h-[100vh] bg-neutral-950 text-white">
+        <h1 className=" text-white lg:text-6xl lg:text-center lg:font-semibold lg:pt-16 font-semibold text-4xl text-center pt-5">
+          Crud Operation
+        </h1>
+        <div className="flex justify-center mt-10 text-white">
+          <button
+            onClick={()=> setShowModal(true)}
+            type="button"
+            className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-md rounded-lg text-xl px-14 py-6 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+          >
+            Add
+          </button>
+
+         
+        </div>
+        <div className="flex justify-center">
+        {showModal && <Popup onClose= {() => setShowModal(false)} />}
+        </div>
+
+
+        <div className="h-[300px] w-full flex justify-center items-center">
+    <div className="h-[300px] w-[900px] mt-10 overflow-y-scroll scrollbar-none">
+    {Array.isArray(data) && data.map((user, index) => (
+      <div key={index}  className="w-full h-16 mb-2 backdrop-blur-sm bg-white/30 flex items-center gap-4 px-5">
+      <input
+          type="checkbox"
+          className="w-[20px] h-[20px]"
+          value={checkBox}
+          onChange={() => setCheckBox(!checkBox)}
+      />
+          <h5>{user?.name}</h5>
+          <h5>{user?.email}</h5>
+          <h5>{user?.phoneNumber}</h5>
+          <h5>{user?.hobbies}</h5>
+          <button onClick={() => handleUpdateClick(user)} className="flex ml-16 items-center justify-center gap-2 bg-purple-700 px-3 py-2 rounded-md"><Pencil/></button>
+          <button onClick = {() => handleDelete(user._id)} className="flex items-center justify-center gap-2 bg-purple-700 px-3 py-2 rounded-md"><Delete/></button>
+          <button onClick = {() => handleSendData(user)} className="flex items-center justify-center gap-2 bg-purple-700 px-3 py-2 rounded-md"><Send/>Send</button>
+        </div>
+    ))}
+
+       
+    </div>
+    {updateModal && <Update user={selectedUser} onClose = {() => setUpdateModal(false)}  />}
+    
+</div>
+    <ToastContainer />
+      
+      </div>
+    </>
+  );
+}
+
+export default App;
+
+
---
+++ client/src/coponents/Popup.jsx
@@ -0,0 +1,106 @@
+import React, { useRef, useState } from 'react'
+import {X, Save} from "lucide-react"
+import axios from "axios"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+<Save />
+function Popup({onClose}) {
+    const modelRef = useRef();
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [hobbies, setHobbies] = useState("");
+
+    const apiUrl = import.meta.env.MODE === 'development'
+    ? import.meta.env.VITE_DEV_API
+    : import.meta.env.VITE_PROD_API;
+
+
+    const closeModal = (e) =>{
+        if(modelRef.current === e.target){
+            onClose();
+        }
+    }
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+      };
+
+      const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+      };
+
+      const handlePhoneNumberChange = (event) => {
+        setPhoneNumber(event.target.value);
+      };
+
+      const handleHobbiesChange = (event) => {
+        setHobbies(event.target.value);
+      };
+
+      const handleSubmit = async() => {
+            const data = {
+                name,
+                email,
+                phoneNumber,
+                hobbies
+            }
+
+            try{
+                const response = await axios.post(`${apiUrl}/api/user`, data);
+                toast("Successfully created")
+            }catch(err){
+                toast("Error during save data")
+            }
+      }
+    
+  return (
+    <div ref= {modelRef} onClick={closeModal} className='fixed z-10 inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center'>
+        <div className=' flex flex-col gap-5 text-white backdrop-blur-sm bg-white/30 p-10 rounded-2xl'>
+            <button onClick={onClose} className='place-self-end'><X size={30} /></button>
+            <div className='w-[350px] ' >
+                <h1 className='text-center mb-5 text-2xl font-bold'>Fill the form</h1>
+                <form className='flex flex-col gap-5 w-full' onSubmit={handleSubmit}>
+                    <input type='text' id='name' 
+                        className='h-10 rounded-md text-gray-700 px-5 text-md font-medium focus:outline-none hover:border-purple-800 focus:ring-4 focus:ring-purple-300'
+                        placeholder='Enter your name'
+                        required
+                        value={name}
+                        onChange={handleNameChange}
+                    />
+                    <input type='email' id='email' 
+                        className='h-10 rounded-md text-gray-700 px-5 text-md font-medium focus:outline-none hover:border-purple-800 focus:ring-4 focus:ring-purple-300'
+                        placeholder='Enter your email'
+                        required
+                        value={email}
+                        onChange={handleEmailChange}
+                    />
+                    <input type='number' id='phoneNumber'
+                        className='h-10 rounded-md text-gray-700 px-5 text-md font-medium focus:outline-none hover:border-purple-800 focus:ring-4 focus:ring-purple-300' 
+                        placeholder='Enter your phone number'
+                        required
+                        value={phoneNumber}
+                        onChange={handlePhoneNumberChange}
+                    />
+                    <input type='text' id='hobbies'
+                        className='h-10 rounded-md text-gray-700 px-5 text-md font-medium focus:outline-none hover:border-purple-800 focus:ring-4 focus:ring-purple-300' 
+                        placeholder='Enter your hobbies'
+                        required
+                        value={hobbies}
+                        onChange={handleHobbiesChange}
+                    />
+
+                    <button className=' flex justify-center items-center py-4 gap-2 font-bold text-2xl focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-md rounded-lg px-14  mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900'><Save/>Save</button>
+                </form>
+                <ToastContainer />
+            </div>
+
+            
+        </div>
+        
+    </div>
+  )
+}
+
+export default Popup
